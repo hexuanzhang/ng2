4f6731824ce805cb43ac296106755d734f936da3
hexuanzhang <136789658@qq.com>
2017-08-30 17:07:45 +0800

    ng-style
    ng-style

---
+++ src/app/directive/ng-style/ng-style.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+	selector: 'ng-style',
+	templateUrl: './ng-style.html',
+	styles: [`
+		span { 
+			display: inline-block;
+			border: 1px solid #ccc;
+			text-align: center;
+			padding: 5px;
+			margin: 10px;
+		}
+	`]
+})
+export class NgStyleComponent implements OnInit {
+	constructor () {}
+	
+	ngOnInit() {}
+	
+	calculateStyles() {
+		return {
+			'width.px': 100
+		};
+	}
+}
