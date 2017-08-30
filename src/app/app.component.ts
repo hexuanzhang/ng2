d317f2ea67f21541a701927106af403848844451
hexuanzhang <136789658@qq.com>
2017-08-30 17:07:28 +0800

    调整项目结构
    调整项目结构

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { menus } from './cofig/menus';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	constructor() {}
+	
+	menu: any[] = menus;
 }
---
+++ src/app/cofig/menus.ts
@@ -0,0 +1,27 @@
+export const menus = [
+	{
+		name: 'directive',
+		children: [
+			{
+				name: 'ng-class'
+			},
+			{
+				name: 'ng-style'
+			},
+			{
+				name: '伪类选择器',
+				children: [
+					{
+						name: ':host'
+					},
+					{
+						name: '::ng-deep'
+					},
+					{
+						name: ':host-context'
+					}
+				]
+			}
+		]
+	}
+];
