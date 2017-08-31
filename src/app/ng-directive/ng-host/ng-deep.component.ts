06b5fd3a9dfd64d0c8d7a391be1ce5d12d5f534f
hexuanzhang <136789658@qq.com>
2017-08-31 17:59:08 +0800

    补充 host 选择器用法
    补充 host 选择器用法

--- src/app/ng-directive/ng-host/ng-deep.component.ts
+++ src/app/ng-directive/ng-host/ng-deep.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
 		<h3>::ng-deep 选择器</h3>
 		<p>应用于组件内所有符合条件的元素，包括子组件符合条件的元素</p>
 	`,
+	styles: [`
+		:host(.active) {
+			margin-top: 5px;
+		}
+	`]
 })
 export class NgDeepComponent {
 	
