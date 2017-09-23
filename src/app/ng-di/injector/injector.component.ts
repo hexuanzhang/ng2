b7ef6797d458396553b7681e90373b9f3413357e
hexuanzhang <136789658@qq.com>
2017-09-23 23:48:06 +0800

    injector
    injector

---
+++ src/app/ng-di/injector/injector.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ReflectiveInjector, Injector, Injectable } from '@angular/core';
+
+@Injectable()
+class Square {
+  name: 'square'
+}
+
+@Injectable()
+class Engine {
+}
+
+@Injectable()
+class Car {
+  constructor(public engine: Engine) {
+  }
+}
+
+@Component({
+  selector: 'app-injector',
+  templateUrl: './injector.html'
+})
+export class InjectorComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    const square_injector: Injector = ReflectiveInjector.resolveAndCreate([Square]),
+          square: Square = square_injector.get(Square);
+    console.info(square instanceof Square, square.name);
+
+    const engine_injector = ReflectiveInjector.resolveAndCreate([Engine]),
+        car_injector: Injector = engine_injector.resolveAndCreateChild([Car]);
+  }
+
+}
