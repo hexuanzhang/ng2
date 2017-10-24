deb920c3612030525b23e7339d6a8cd17c84bd4c
hexuanzhang <136789658@qq.com>
2017-10-24 11:59:58 +0800

    animationBuilder
    animationBuilder

--- src/app/ng-animation/loading/loading.component.ts
+++ src/app/ng-animation/loading/loading.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AnimationBuilder, style, transition, animate, keyframes,
+  stagger, AnimationPlayer
+} from '@angular/animations';
 import { Loading01Animations, Loading02Animations, Loading04Animations, ListAnimations } from '../animation-config';
 
 @Component({
-	selector: 'loading',
-	templateUrl: './loading.html',
+  selector: 'loading',
+  templateUrl: './loading.html',
   styleUrls: ['./loading.scss'],
   animations: [
     Loading01Animations,
@@ -13,28 +17,41 @@ import { Loading01Animations, Loading02Animations, Loading04Animations, ListAnim
   ]
 })
 export class LoadingComponent implements OnInit {
+  constructor(public builder: AnimationBuilder) {
+  }
 
-	constructor() {
-	}
+  @ViewChild('box06') box06;
+  player: AnimationPlayer;
 
   loadingStatus: string = '';
   items: number[] = [];
+  items02: number[] = [];
 
-	ngOnInit() {
-	}
+  ngOnInit() {
+  }
 
   ngAfterViewInit() {
-	  this.items = [1, 2, 3, 4, 5];
+    this.items = this.items02 = [1, 2, 3, 4, 5];
   }
 
   toggleStatus() {
-	  const r: number = Math.ceil(Math.random() * 100);
-    this.loadingStatus = this.loadingStatus === 'start' ? 'end': 'start';
-    this.items = [];
+    const r: number = Math.ceil(Math.random() * 100);
+    this.loadingStatus = this.loadingStatus === 'start' ? 'end' : 'start';
+    this.items = this.items02 = [];
 
     setTimeout(() => {
-      this.items = [r + 1, r + 2, r + 3, r + 4, r + 5];
+      this.items = this.items02 = [r + 1, r + 2, r + 3, r + 4, r + 5];
     }, 500);
 
+    const factory = this.builder.build([
+      animate("1s ease-out", keyframes([
+        style({ transform: "scale(0)", opacity: "0", offset: 0}),
+        style({ transform: "scale(.5)", opacity: "1", offset: 0.5} ),
+        style({ transform: "scale(1)", opacity: "0", offset: 0.75} ),
+        style({ transform: "scale(1)", opacity: "1", offset: 1} )
+      ]))
+    ]);
+    this.player = factory.create(this.box06.nativeElement,{});
+    this.player.play();
   }
 }
--- src/app/ng-animation/ng-animation.component.ts
+++ src/app/ng-animation/ng-animation.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, animate, animateChild, transition, keyframes, style} from '@angular/animations';
 
 @Component({
   selector: 'ng-animation',
-  templateUrl: './ng-animation.html'
+  templateUrl: './ng-animation.html',
+  animations: [
+    trigger('routeAnimation', [
+      transition('void => *', [
+        animate('0.5s ease-out', keyframes([
+          style({opacity: 0, offset: 0}),
+          style({opacity: 0.25, offset: 0.5}),
+          style({opacity: 0.5, offset: 1})
+        ]))
+        //animateChild()
+      ], { delay: '10s'})
+    ])
+  ]
 })
 export class NgAnimationComponent {
   constructor() {
+  }
+
+  getRouteAnimation(outlet: any) {
+    return 'firstPage';
   }
 }
