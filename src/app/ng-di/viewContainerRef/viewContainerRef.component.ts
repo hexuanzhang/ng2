86e47594a5c7c7689f8b6055fd39b7dd78899f2d
hexuanzhang <136789658@qq.com>
2017-10-31 20:51:13 +0800

    ViewContainerRef
    ViewContainerRef demo

--- src/app/ng-di/viewContainerRef/viewContainerRef.component.ts
+++ src/app/ng-di/viewContainerRef/viewContainerRef.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit, ReflectiveInjector, Injector, Injectable } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'viewContainerRef',
   templateUrl: './viewContainerRef.html'
 })
 export class ViewContainerRefComponent implements OnInit {
-
-  constructor() {
+  @ViewChild('tpl') tpl;
+  @ViewChild('container', { read: ViewContainerRef}) _vcr: ViewContainerRef;
+  
+  /**
+   *  ViewContainerRef：将新组件或元素插入至容器后方（相邻）
+   *
+   */
+  constructor(private vcr:ViewContainerRef) {
+    console.info('constructor：', this.tpl);
   }
 
   ngOnInit() {
-    console.info('viewContainerRef');
+    console.info('ngOnInit：', this.tpl);
+    
+    // this.vcr 指向 host 元素
+    this.vcr.createEmbeddedView(this.tpl);
+    this._vcr.createEmbeddedView(this.tpl);
+  }
+  
+  ngAfterViewInit() {
+    console.info('ngAfterViewInit：', this.tpl);
   }
 
 }
