a2f22f1850017af8c207220825092e3c06ae7c07
hexuanzhang <136789658@qq.com>
2017-12-12 18:14:04 +0800

    remove opaqueToken
    OpaqueToken will be removed in angular 5

--- src/app/ng-provider/opaqueToken/opaqueToken.component.ts
+++ src/app/ng-provider/opaqueToken/opaqueToken.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit, Inject, OpaqueToken } from '@angular/core';
+/**
+ *  OpaqueToken will be removed in angular 5
+ *
+ *  https://github.com/SebastianM/angular-google-maps/issues/1135
+ *  https://alligator.io/angular/angular-5/
+ */
+
+// import { Component, OnInit, Inject, OpaqueToken } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
 import { FEATURE_ENABLED, APP_INFO } from './config';
 
-const PROVIDE_TOKENS = [new OpaqueToken('app_info'), new OpaqueToken('feature_enabled')];
+// const PROVIDE_TOKENS = [new OpaqueToken('app_info'), new OpaqueToken('feature_enabled')];
+const PROVIDE_TOKENS = ['app_info', 'feature_enabled'];
 
 @Component({
 	selector: 'opaqueToken',
@@ -23,7 +33,7 @@ export class OpaqueTokenComponent implements OnInit {
 	};
 	
 	ngOnInit() {
-		console.info(new OpaqueToken('test') === new OpaqueToken('test'));
+		// console.info(new OpaqueToken('test') === new OpaqueToken('test'));
 		
 		this.featureEnabled && (this.app = this.appInfo);
 	}
