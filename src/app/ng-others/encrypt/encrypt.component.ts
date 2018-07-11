21224bc27fb8fee6775d454922edaa90b5b981ac
hexuanzhang <136789658@qq.com>
2018-07-11 18:44:23 +0800

    update encrypt module

--- src/app/ng-others/encrypt/encrypt.component.ts
+++ src/app/ng-others/encrypt/encrypt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { privateKey, publicKey } from './config';
+import { JSEncrypt } from 'jsencrypt';
 
 @Component({
 	selector: 'app-encrypt',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncryptComponent implements OnInit {
 
+	str: string = ''; // 明文
+	encryptStr: string = ''; // 加密后的密文
+
+	_encrypt: any; // JSEncrypt 实例
+
 	constructor() { }
 
 	ngOnInit() {
+		this._encrypt = new JSEncrypt();
+
+	}
+
+	encrypt() {
+		this._encrypt.setPublicKey(publicKey);
+		this.encryptStr = this._encrypt.encrypt(`${this.str}`.trim());
+	}
+
+	decrypt() {
+		this._encrypt.setPrivateKey(privateKey);
 	}
 
 }
